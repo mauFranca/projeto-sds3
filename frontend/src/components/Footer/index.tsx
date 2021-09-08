6d7ce40123553653c349feb81b972250278cb6e2
Mauricio Franca <mauriciofrancadelima@gmail.com>
2021-09-08 18:34:49 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/mauFranca" target="_blank" rel="noreferrer">Mauricio Franca</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/mau.franca/" target="_blank" rel="noreferrer">@mau.franca</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
